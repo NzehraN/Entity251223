7e7549d3ea9760c260bbc50bd238a797d4449cbb
Zehranalci <zahre1204@gmail.com>
2023-12-25 19:32:43 +0300

    Entity251223

---
+++ Ent251223/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ent251223.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Ent251223/Models/SkillsValidateAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Ent251223.Models
+{
+    internal class SkillsValidateAttribute : Attribute
+    {
+        public string[] Allowed { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
